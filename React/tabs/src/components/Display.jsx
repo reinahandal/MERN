62b6b7aaa07c3df43754cd0022bbf5bd67c6912d
reinahandal <handalreina@gmail.com>
2022-12-11 17:05:20 +0200

    doesnt show display div unless there is a selected tab

--- React/tabs/src/components/Display.jsx
+++ React/tabs/src/components/Display.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 const Display = (props) => {
     return (
+        props.selectedTab ?
         <div style={{
             border:"2px solid gray",
             padding:"15px",
             margin:"0 20px"
         }}>
             {props.selectedTab.content}
-        </div>
+        </div> : ''
     )
 }
 
