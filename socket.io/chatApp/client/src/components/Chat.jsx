ada8eb9d79c31c8e5f11587ed609f39f879f64dd
reinahandal <handalreina@gmail.com>
2022-12-23 20:04:49 +0200

    styling done

--- socket.io/chatApp/client/src/components/Chat.jsx
+++ socket.io/chatApp/client/src/components/Chat.jsx
@@ -1,11 +1,18 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import io from 'socket.io-client';
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 
 const Chat = (props) => {
     const [socket] = useState(()=> io(':8000'));
     const [user, SetUser] = useState(props.currentUser);
     const [message, setMessage] = useState("");
     const [messages, setMessages] = useState([]);
+    const bottomRef = useRef(null);
+
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({behavior: 'smooth'});
+    }, [messages]);
 
     useEffect(()=> {
         socket.on("new_msg", allMsgs => {
@@ -19,21 +26,30 @@ const Chat = (props) => {
         socket.emit("send_msg", {txt:message,sender:user});
         setMessage("");
     }
+
+    const chatStyle = {
+        marginLeft: 'auto',
+        
+    }
     return (
-        <div style={{margin:"0px 5%"}}>
-        <h1>Hi, {user}!</h1>
-        <div style={{overflowY:"scroll", height:"400px", backgroundColor:"lightgray"}}>
+        <div style={{margin:"5%"}}>
+        <h1 className='mb-4'>Hi, {user}!</h1>
+        <div style={{overflowY:"scroll", height:"400px", backgroundColor:"#f2f3f4", position:"relative"}}>
         {messages.map((msg, i)=> <div 
         style={{backgroundColor:"white", 
         padding:"10px", 
-        margin:"10px", 
         width:"50%", 
         borderRadius:"20px",
-        }}><span style={{color:"red",fontWeight:"bold"}}>{msg.sender}:</span> {msg.txt}</div>)}
+        margin:"10px 0",
+        marginLeft: msg.sender===user ? 'auto':'10px',
+        marginRight: msg.sender===user ? '10px':''
+        }}
+        ><span className='text-primary font-weight-bold'>{msg.sender===user ? "You": msg.sender}</span>: {msg.txt}</div>)}
+        <div ref={bottomRef} />
         </div>
-        <form onSubmit={sendMsg}>
-            <input type="text" onChange={e=>setMessage(e.target.value)} value={message}></input>
-            <input type="submit" value="Send"></input>
+        <form className='mt-3' onSubmit={sendMsg}>
+            <TextField id="outlined-basic" variant="outlined" size="small" sx={{width:'85%'}} onChange={e=>setMessage(e.target.value)} value={message}/>
+            <Button variant="contained" type="submit"sx={{ml:'10px'}}>Send</Button>
         </form>
         </div>
     )
--- socket.io/chatApp/client/src/components/ChatForm.jsx
+++ socket.io/chatApp/client/src/components/ChatForm.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
-
+import Paper from '@mui/material/Paper'
+import Container from '@mui/material/Container'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
 const ChatForm = (props) => {
     const [user, setUser] = useState();
 
@@ -10,14 +13,16 @@ const ChatForm = (props) => {
         console.log(user)
     }
     return (
-        <div>
-            <h1>Get started right now!</h1>
+        <Container maxWidth="sm" component={Paper} sx={{mx:'auto', my:'10%', p:'5%' }} elevation={2}>
+            <h1 className="text-center">Get started right now!</h1>
         <form onSubmit={handleSubmit}>
-            <label>I want to start chatting with the name...</label><br/>
-            <input type="text" value={user} onChange={e=>setUser(e.target.value)}></input>
-            <input type="submit" value="Start Chatting"></input>
+            <p className='my-3 text-center'>I want to start chatting with the name...</p>
+            <div style={{display: "flex", alignItems:"center", justifyContent:"center", padding:"5px"}}>
+                <TextField id="outlined-basic" label="My name..." size="small" variant="outlined"  value={user} onChange={e=>setUser(e.target.value)}/>
+                <Button variant="contained"type="submit" color="success" sx={{ml:'10px'}}>Start Chatting</Button>
+            </div>
         </form>
-        </div>
+        </Container>
     )
 }
 
